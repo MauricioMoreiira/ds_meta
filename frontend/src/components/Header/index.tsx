d69728c9ad3316dae875219af2de90fbdc94598f
Mauricio Moreira <mauricionetos182@gmail.com>
2022-07-16 18:29:19 -0300

    Outros componentes

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import icon from '../../assets/img/logo.svg'
+import './styles.css'
+function Header() {
+    return (
+        <>
+            <header>
+                <div className="dsmeta-logo-container">
+                    <img src= {icon} alt="DSMeta" />
+                    <h1>DSMeta</h1>
+                    <p>
+                        Desenvolvido por
+                        <a href="https://github.com/MauricioMoreiira"> @MauMoreiira</a>
+                    </p>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Header
